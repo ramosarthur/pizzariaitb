6a70bb3aba96d638f77315caf4c249b7bea17fa8
ramosarthur <159132714+ramosarthur@users.noreply.github.com>
2024-08-02 20:48:24 -0300

    tudo acabará em pizza.

--- src/assets/pages/Listagem.jsx
+++ src/assets/pages/Listagem.jsx
@@ -1,10 +1,32 @@
 import React, {useState, useEffect} from 'react'
+import pizzas from '../shared/pizzas'
 
 const Listagem = () => {
 
+    const ItensLista = () => pizzas.map(
+        pizza => 
+        <li style={{listStyle: 'none'}}>
+           <img src={pizza.imagem} 
+           alt={pizza.nome}
+           style={{
+            width: "150",
+            height: "100px",
+            borderRadius: "8px",
+            margin: "0px",
+            }}
+            /> 
+        </li>
+    )
+
     return (
         <>
-        <h2>Pizzas ITB</h2>
+            <h2 style={{textAlign: 'center'}}>
+                Pizzas ITB
+            </h2>
+            <ul>
+                 <ItensLista />
+            </ul>
+           
         </>
     )
 }

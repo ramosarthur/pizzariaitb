58a859e6c6dcb838e25aef1f50c74a458e965443
ramosarthur <159132714+ramosarthur@users.noreply.github.com>
2024-08-02 19:45:27 -0300

    mesage

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
+import {useState} from 'react'
 import './App.css'
 import { Routes, Route } from 'react-router-dom'
+import Listagem from './assets/pages/Listagem'
 
 const App = () => {
 
+  const [color] = useState("#0000FF")
+  document.body.style.backgroundColor = color  
   return (
     <Routes>
-      <Route path="/" />
+      <Route path="/" element={<Listagem  />} />
     </Routes>
   )
 }

b04922bec24df9387f8a15bb3ef6a17e8d49bc9e
ramosarthur <159132714+ramosarthur@users.noreply.github.com>
2024-08-03 21:01:41 +0000

    tudo acabará em pizza

--- src/shared/Pizzas.jsx
+++ src/shared/Pizzas.jsx
@@ -7,35 +7,33 @@ import calabresa from "../assets/imagens/calabresa.png"
 const Pizzas = [ 
     {
     "id": 1,
-    "nome":"Margherita",
+    "nome":"atum",
     "descrição":
     "Pizza clássica com molho de tomate, mozzarella e manjericão.",
     "imagem": margherita
     },
     {
     "id": 2,
-    "nome":"Pepperoni",
+    "nome":"calabresa",
     "descrição":"Pizza com molho de tomate, mozzarella e fatias de pepperoni.",
     "imagem": pepperoni
     },
     {
     "id": 3,
-    "nome" :
-    "Quattro Formaggi",
+    "nome" : "frango",
     "descrição":"Pizza com uma mistura de quatro queijos: mozzarella, gorgonzola, parmesão e fontina",
     "imagem": quattroformaggi
     },
     {
     "id": 4,
-    "nome":"Hawaiana",
+    "nome":"mussarela",
     "descrição":
     "Pizza com molho de tomate, mozzarella, presunto e abacaxi.",
     "imagem": hawaiana
     },
     {
     "id": 5,
-    "nome":
-    "Calabresa",
+    "nome": "portuguesa",
     "descrição":"Pizza com molho de tomate, mozzarella e linguiça calabresa.",
     "imagem": calabresa
     },
